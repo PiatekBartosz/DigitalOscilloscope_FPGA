8243cca41e75069495e5fa9cac2e3d48f7672493
Bartosz Piątek <bartekpiatek123@gmail.com>
2025-09-10 00:24:01 +0200

    Add pll 80Mhz clock output for ADC

---
+++ digital_oscilloscope.cdf
@@ -0,0 +1,13 @@
+/* Quartus Prime Version 24.1std.0 Build 1077 03/04/2025 SC Lite Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(EP4CE22) Path("/home/bartek/workspace/DigitalOscilloscope/DigitalOscilloscope_FPGA/") File("output_file.jic") MfrSpec(OpMask(1) SEC_Device(EPCS64) Child_OpMask(1 1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
--- src/digital_oscilloscope.sv
+++ src/digital_oscilloscope.sv
@@ -1,11 +1,21 @@
 module digital_oscilloscope (
-    input  clk,
-    output led
+    input  clk_in_0,
+    output led,
+    output clk_out_0,
+    output clk_out_1
 );
 
-    blinky blinky_inst (
-        .clk(clk),
-        .led(led)
-    );
+blinky blinky_inst (
+    .clk(clk_in_0),
+    .led(led)
+);
+
+adc_clk adc_clk_inst (
+	.areset(1'b0),
+	.inclk0(clk_in_0),
+	.c0(clk_out_0),
+	.c1(clk_out_1),
+	.locked(pll_locked)
+);
 
 endmodule
